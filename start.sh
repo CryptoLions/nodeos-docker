847de3e5e999017a9d8b3be34ca35f2470bc8632
Bohdan Cryptolions <37701692+ansigroup@users.noreply.github.com>
2018-04-17 01:45:13 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+################################################################################
+#
+# Scrip prepared by http://CryptoLions.io
+# https://github.com/CryptoLions/EOS-Jungle-Testnet
+#
+###############################################################################
 
-docker service create --name nodeos --replicas 1 -p 8888:8888 -p 9876:9876 --mount type=bind,source=/opt/DockerJungleNet/"{{.Task.Slot}}",destination=/opt/eosio/bin/data-dir -t eosio/eos nodeosd.sh
+WORKERSDIR="/opt/DockerJungleNet"
+TAG="latest"
+HTTP_PORT="8888:8888"
+P2P_PORT="9876:9876"
+
+docker service create --name nodeos --replicas 1 -p $HTTP_PORT -p $P2P_PORT --mount type=bind,source=$WORKERSDIR/"{{.Task.Slot}}",destination=/opt/eosio/bin/data-dir -t eosio/eos":"$TAG nodeosd.sh
