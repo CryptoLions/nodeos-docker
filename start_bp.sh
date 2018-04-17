2e5c65c464825542441f0d90f9cbd3a00e2691b6
Bohdan Cryptolions <37701692+ansigroup@users.noreply.github.com>
2018-04-17 21:37:25 +0300

    Create start_bp.sh

---
+++ start_bp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+################################################################################
+#
+# Scrip prepared by http://CryptoLions.io
+# https://github.com/CryptoLions/EOS-Jungle-Testnet
+#
+###############################################################################
+
+WORKERSDIR="/opt/DockerJungleNet"
+TAG="v300"
+HTTP_PORT="3888:3888"
+P2P_PORT="4876:4876"
+
+docker service create --name nodeos --replicas 1 -p $HTTP_PORT -p $P2P_PORT --mount type=bind,source=$WORKERSDIR/producer,destination=/opt/eosio/bin/data-dir -t eosio/eos":"$TAG nodeosd.sh
+
+
+
