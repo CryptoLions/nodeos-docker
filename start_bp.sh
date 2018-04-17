e4f5be2c2d5b75c58890ddf3b1da4517a04892c1
Bohdan Cryptolions <37701692+ansigroup@users.noreply.github.com>
2018-04-17 21:51:52 +0300

    Update start_bp.sh

--- start_bp.sh
+++ start_bp.sh
@@ -11,7 +11,7 @@ TAG="v300"
 HTTP_PORT="3888:3888"
 P2P_PORT="4876:4876"
 
-docker service create --name nodeos --replicas 1 -p $HTTP_PORT -p $P2P_PORT --mount type=bind,source=$WORKERSDIR/producer,destination=/opt/eosio/bin/data-dir -t eosio/eos":"$TAG nodeosd.sh
+docker service create --name nodeos_bp --replicas 1 -p $HTTP_PORT -p $P2P_PORT --mount type=bind,source=$WORKERSDIR/producer,destination=/opt/eosio/bin/data-dir -t eosio/eos":"$TAG nodeosd.sh
 
 
 
