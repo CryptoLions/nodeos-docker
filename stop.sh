47b909333d3110b5effba728a6687d8848afbf5e
Bohdan Cryptolions <37701692+ansigroup@users.noreply.github.com>
2018-04-17 23:50:27 +0300

    Create stop.sh

---
+++ stop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+################################################################################
+#
+# Scrip prepared by http://CryptoLions.io
+# https://github.com/CryptoLions/EOS-Jungle-Testnet
+#
+###############################################################################
+
+
+docker service rm nodeos
